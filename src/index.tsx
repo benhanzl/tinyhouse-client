e05ed7818cc72467ea3d5fb32651e22487a110a4
Ben Hanzl <ben@benhanzl.com>
2019-11-22 14:16:20 -0600

    Create a Listings component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
+import { Listings } from "./sections";
 import * as serviceWorker from "./serviceWorker";
 
-render(<div>Hello World!</div>, document.getElementById("root"));
+render(<Listings />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
