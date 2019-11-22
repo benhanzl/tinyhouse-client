5f275234a1644a790813459a0578d62760ccedf6
Ben Hanzl <ben@benhanzl.com>
2019-11-22 15:13:44 -0600

    Delete a listing through GraphQL

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { server } from "../../lib/api";
-import { ListingsData } from "./types";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData
+} from "./types";
 
 const LISTINGS = `
   query Listings {
@@ -18,20 +22,44 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
   const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    console.log(data.listings);
+    const { data } = await server.fetch<ListingsData>({
+      query: LISTINGS
+    });
+    console.log(data);
+  };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5d4507a9cf295034813b35c2"
+      }
+    });
+    console.log(data);
   };
 
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete a listing!</button>
     </div>
   );
 };
