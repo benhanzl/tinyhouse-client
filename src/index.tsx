4b81d5ded088f82caf0c94f90673a86d76cf39fa
Ben Hanzl <ben@benhanzl.com>
2020-02-08 13:03:59 -0600

    Add React Router and routes to sections

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import ApolloClient from "apollo-boost";
 import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { render } from "react-dom";
-import { Listings } from "./sections";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
 
@@ -10,9 +11,24 @@ const client = new ApolloClient({
   uri: "/api"
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
 render(
   <ApolloProvider client={client}>
-    <Listings title="TinyHouse Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 );
