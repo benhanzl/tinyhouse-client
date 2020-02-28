1da9795dc08e7b20c794b0de04f93151482cebbf
Ben Hanzl <ben@benhanzl.com>
2020-02-28 14:28:54 -0600

    Perform the log in and handle errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ApolloClient from "apollo-boost";
-import React from "react";
+import React, { useState } from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Layout } from "antd";
+import { Viewer } from "./lib/types";
 import {
   Home,
   Host,
@@ -20,7 +21,17 @@ const client = new ApolloClient({
   uri: "/api"
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+
   return (
     <Router>
       <Layout id="app">
@@ -29,7 +40,11 @@ const App = () => {
           <Route exact path="/host" component={Host} />
           <Route exact path="/listing/:id" component={Listing} />
           <Route exact path="/listings/:location?" component={Listings} />
-          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/login"
+            render={props => <Login {...props} setViewer={setViewer} />}
+          />
           <Route exact path="/user/:id" component={User} />
           <Route component={NotFound} />
         </Switch>
