e52b6feb294df5100149158f2120224fb8ee9013
Ben Hanzl <ben@benhanzl.com>
2020-02-28 13:54:30 -0600

    Add Login component

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,16 @@ import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
+import { Layout } from "antd";
+import {
+  Home,
+  Host,
+  Listing,
+  Listings,
+  Login,
+  NotFound,
+  User
+} from "./sections";
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
 
@@ -14,14 +23,17 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
