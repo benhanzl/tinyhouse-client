141aa5e2fbcd82517834f42b2956ada5c4db129c
Ben Hanzl <ben@benhanzl.com>
2020-03-04 16:34:47 -0600

    Move authorize to the correct project

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,4 @@
-import { Request } from "express";
 import { message, notification } from "antd";
-import { Database, User } from "../types";
 
 export const displaySuccessNotification = (
   message: string,
@@ -18,17 +16,4 @@ export const displaySuccessNotification = (
 
 export const displayErrorMessage = (error: string) => {
   return message.error(error);
-};
-
-export const authorize = async (
-  db: Database,
-  req: Request
-): Promise<User | null> => {
-  const token = req.get("X-CSRF-TOKEN");
-  const viewer = await db.users.findOne({
-    _id: req.signedCookies.viewer,
-    token
-  });
-
-  return viewer;
 };
